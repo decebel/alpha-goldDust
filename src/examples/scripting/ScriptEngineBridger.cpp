296bb836854c24f55996f9f2c951e53c14bcb955
Decebel <decebel@gmail.com>
2013-04-21 22:37:45 -0400

    register module name with init prefix

--- src/examples/scripting/ScriptEngineBridger.cpp
+++ src/examples/scripting/ScriptEngineBridger.cpp
@@ -8,16 +8,16 @@ namespace python = boost::python;
 class Base : public boost::noncopyable
 {
 public:
-  virtual ~Base() {};
-  virtual std::string hello() = 0;
+	virtual ~Base() {};
+	virtual std::string hello() = 0;
 };
 
 // C++ derived class
 class CppDerived : public Base
 {
 public:
-  virtual ~CppDerived() {}
-  virtual std::string hello() { return "Hello from C++!";}
+	virtual ~CppDerived() {}
+	virtual std::string hello() { return "Hello from C++!";}
 };
 
 
@@ -25,18 +25,18 @@ public:
 // Familiar Boost.Python wrapper class for Base
 struct BaseWrap : Base, python::wrapper<Base>
 {
-  virtual std::string hello() 
-  {
+	virtual std::string hello() 
+	{
 #if BOOST_WORKAROUND(BOOST_MSVC, <= 1300)
-    // workaround for VC++ 6.x or 7.0, see
-    // http://boost.org/libs/python/doc/tutorial/doc/html/python/exposing.html#python.class_virtual_functions
-    return python::call<std::string>(this->get_override("hello").ptr());
+		// workaround for VC++ 6.x or 7.0, see
+		// http://boost.org/libs/python/doc/tutorial/doc/html/python/exposing.html#python.class_virtual_functions
+		return python::call<std::string>(this->get_override("hello").ptr());
 #else
-	std::string s("hello");
-	return python::call<std::string>(this->get_override("hello").ptr());
-	//return this->get_override("hello")();
+		std::string s("hello");
+		return python::call<std::string>(this->get_override("hello").ptr());
+		//return this->get_override("hello")();
 #endif
-  }
+	}
 };
 
 /// 
@@ -89,7 +89,7 @@ struct SpringBoardDataManager {
 	static void error_message(const std::string& msg_, const std::string& sender_) {
 
 	}
-	
+
 	static std::vector<std::string> get_all_market_names() {
 
 		std::vector<std::string> v;
@@ -140,10 +140,10 @@ struct SpringBoardDataManager {
 // Pack the Base class wrapper into a module
 BOOST_PYTHON_MODULE(embedded_hello)
 {
-  python::class_<BaseWrap, boost::noncopyable> base("Base");
+	python::class_<BaseWrap, boost::noncopyable> base("Base");
 }
 
-BOOST_PYTHON_MODULE(sb_datamanager)
+BOOST_PYTHON_MODULE(sbdatamanager)
 {
 	python::class_<SpringBoardDataManager, boost::noncopyable>("SpringBoardDataManager")
 		.def("version", &SpringBoardDataManager::version)
@@ -173,53 +173,53 @@ BOOST_PYTHON_MODULE(sb_datamanager)
 
 void exec_test()
 {
-    std::cout << "registering extension module embedded_hello..." << std::endl;
-  
-  // Initialize the interpreter
-  Py_Initialize();
+	std::cout << "registering extension module embedded_hello..." << std::endl;
 
-  // Register the module with the interpreter
-  //if (PyImport_AppendInittab("embedded_hello", initembedded_hello) == -1)
-  //  throw std::runtime_error("Failed to add embedded_hello to the interpreter's "
-  //               "builtin modules");
+	// Initialize the interpreter
+	Py_Initialize();
 
-  std::cout << "defining Python class derived from Base..." << std::endl;
-  
-  // Retrieve the main module
-  python::object main = python::import("__main__");
-  
-  // Retrieve the main module's namespace
-  python::object global(main.attr("__dict__"));
+	// Register the module with the interpreter
+	if (PyImport_AppendInittab("embedded_hello", initembedded_hello) == -1)
+		throw std::runtime_error("Failed to add embedded_hello to the interpreter's "
+		"builtin modules");
 
-  // Define the derived class in Python.
-  python::object result = python::exec(
-    "from embedded_hello import *        \n"
-    "class PythonDerived(Base):          \n"
-    "    def hello(self):                \n"
-    "        return 'Hello from Python!' \n",
-    global, global);
+	std::cout << "defining Python class derived from Base..." << std::endl;
+
+	// Retrieve the main module
+	python::object main = python::import("__main__");
+
+	// Retrieve the main module's namespace
+	python::object global(main.attr("__dict__"));
+
+	// Define the derived class in Python.
+	python::object result = python::exec(
+		"from embedded_hello import *        \n"
+		"class PythonDerived(Base):          \n"
+		"    def hello(self):                \n"
+		"        return 'Hello from Python!' \n",
+		global, global);
 
 
-  // Define the plugin manager class in Python.
-  python::object result_sbdm = python::exec(
-    "from sb_datamanager import SpringBoardDataManager   \n",
-    global, global);
+	// Define the plugin manager class in Python.
+	python::object result_sbdm = python::exec(
+		"from sbdatamanager import SpringBoardDataManager   \n",
+		global, global);
 
-  python::object PythonDerived = global["PythonDerived"];
+	python::object PythonDerived = global["PythonDerived"];
 
-  // Creating and using instances of the C++ class is as easy as always.
-  CppDerived cpp;
-  //BOOST_TEST(cpp.hello() == "Hello from C++!");
+	// Creating and using instances of the C++ class is as easy as always.
+	CppDerived cpp;
+	//BOOST_TEST(cpp.hello() == "Hello from C++!");
 
-  std::cout << "testing derived class from C++..." << std::endl;
-  
-  // But now creating and using instances of the Python class is almost
-  // as easy!
-  python::object py_base = PythonDerived();
-  Base& py = python::extract<Base&>(py_base) BOOST_EXTRACT_WORKAROUND;
+	std::cout << "testing derived class from C++..." << std::endl;
 
-  // Make sure the right 'hello' method is called.
-  //BOOST_TEST(py.hello() == "Hello from Python!");
+	// But now creating and using instances of the Python class is almost
+	// as easy!
+	python::object py_base = PythonDerived();
+	Base& py = python::extract<Base&>(py_base) BOOST_EXTRACT_WORKAROUND;
+
+	// Make sure the right 'hello' method is called.
+	//BOOST_TEST(py.hello() == "Hello from Python!");
 
 	std::string script_ = "C:\\Amit\\dev\\cpp\\of_v0.7.4_vs2010_release\\of_v0.7.4_vs2010_release\\apps\\myApps\\alpha-goldDust\\bin\\data\\packages\\springboard.py";
 	python::object fileresult = python::exec_file(script_.c_str(), global, global);
@@ -236,7 +236,7 @@ void exec_test()
 	//int len = python::extract<int>(input_dd_objects.attr("count"));
 	std::cout<<"len = "<<val<<std::endl;
 
-  std::cout << "success!" << std::endl;
+	std::cout << "success!" << std::endl;
 }
 
 ScriptEngineBridger& ScriptEngineBridger::instance() {
@@ -247,25 +247,34 @@ ScriptEngineBridger& ScriptEngineBridger::instance() {
 class Scope {
 
 public:
-	
+
 	python::object main;
 	python::object main_dict;
 	python::object datamanager;
+	python::object result_sbdm;
 
 	void initialize() {
 
 		// Retrieve the module
 		main = python::import("__main__");
-  
+
 		// Retrieve the module's namespace
 		main_dict  = (main.attr("__dict__"));
 
 		// Register the module with the interpreter
-		if (PyImport_AppendInittab("sbdatamanager", sb_datamanager) == -1)
+		if (PyImport_AppendInittab("sbdatamanager", initsbdatamanager) == -1)
 			throw std::runtime_error("Failed to add sbdatamanager to the interpreter's "
-                 "builtin modules");
+			"builtin modules");
 
 		std::cout << "defining Python class sbdatamanager ..." << std::endl;
+
+
+		// Define the plugin manager class in Python.
+		result_sbdm = python::exec(
+			"from sbdatamanager import SpringBoardDataManager   \n",
+			main_dict, main_dict);
+
+
 
 
 	}
@@ -294,7 +303,7 @@ public:
 	ScriptEngine() {}
 
 	void initialize() {
-		
+
 	}
 
 	void testEngine() {
