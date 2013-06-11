f766c67c7ff6c4e98cecbd86025cc79b37a480b5
Decebel <decebel@gmail.com>
2013-06-10 23:11:16 -0400

    changes for a working plugin. loaded via api framework

--- bin/plug/py/api/drawingboard.py
+++ bin/plug/py/api/drawingboard.py
@@ -4,11 +4,15 @@ Engine shall instantiate the drawingboard module.
 """
 
 import os
-import sys
+plugSourcesPath = os.path.join("..", "sources")
+webplugSourcesPath = os.path.join(plugSourcesPath, "weby")
+import sys; sys.path.insert(0, webplugSourcesPath)
+
 import pprint
 import imp
 pp = pprint.PrettyPrinter(indent=4)
 module_plugins = []
+plugin_objects = []
 
 class DrawingBoardInitError(Exception):
 	pass
@@ -72,7 +76,14 @@ class drawingboard(object):
 
 # TODO - doc strings and group the apis based on usage. caller etc
 class DataCommand(object):
+
 	is_command = True
+
+	def __init__(self, **kwargs):
+		print "DataCommand init called"
+
+	def load_resources(self, **kwargs):
+		pass
 
 	def set_display_name(self, name):
 		pass
@@ -86,16 +97,20 @@ class DataCommand(object):
 	def set_status_string(self):
 		pass
 
+
 	def set_icon(self, resource):
 		pass
 
 	def get_icon(self):
 		pass
 
-	def init(self):
+	def init(self, **kwargs):
 		pass
 
 	def is_initialized(self):
+		pass
+
+	def start(self, **kwargs):
 		pass
 
 	def stop(self):
@@ -166,7 +181,10 @@ class DataCommand(object):
 	def get_input_port_schema(self, port):
 		pass
 
-	def submit_command(self, port, commandArgs):
+	def submit_command(self, port, **commandArgs):
+		pass
+
+	def execute(self, **kwargs):
 		pass
 
 	def send_output(self, port, data):
@@ -208,14 +226,14 @@ class DataCommand(object):
 	def is_resumed(self):
 		pass
 
-
-
-def list_directory(directory, fileExt = ".py"):
+def list_directory(directory, fileExt = [".py"]):
 	fileList = [f for f in os.listdir(directory)]
 	fileList = [os.path.splitext(f)[0] for f in fileList if os.path.splitext(f)[1] in fileExt]
+	print "fileList {0}".format(fileList)
 	return fileList
 
-def load_data_plugins(self, directory, fileExt = ".py"):
+
+def load_data_plugins(directory, fileExt = [".py"]):
 	modNames = list_directory(directory, fileExt)
 
 	for moduleName in modNames:
@@ -223,7 +241,7 @@ def load_data_plugins(self, directory, fileExt = ".py"):
 		if moduleName == "drawingboard":
 			print "Skipping module {0}".format(moduleName)
 			continue
-		m_info = imp.find_module(moduleName, ["."])
+		m_info = imp.find_module(moduleName)
 		m = imp.load_module(moduleName, *m_info)
 
 		for name in dir(m):
@@ -235,7 +253,8 @@ def load_data_plugins(self, directory, fileExt = ".py"):
 							print "Found plugin class: {0}".format(name)
 							module_plugins.append(t)
 					except AttributeError:
-						print "class: {0} not a plugin class".format(name)
+						pass
+						#print "class: {0} not a plugin class".format(name)
 			except AttributeError:
 				pass
 
@@ -243,16 +262,27 @@ def load_data_plugins(self, directory, fileExt = ".py"):
 def create_data_commands():
 	cmds = []
 	for cls in module_plugins:
-		cmds.append(cls())
+		pluginObject = cls(dummy="dummy")
+		pluginObject.start(en="english")
+		cmds.append(pluginObject)
 	return cmds
 
+def testExecute():
+	for pluginObject in plugin_objects:
+		pluginObject.execute(search = "now is the time")
+	
+
 def main():
-	#load_data_plugins(".", ".py")
-	#create_data_commands()
-	pass
+	pp.pprint(sys.path)
+
+	webPlugPathTest = os.path.join(plugSourcesPath, "weby")
+	load_data_plugins(webPlugPathTest)
+	cmds = create_data_commands()
+	plugin_objects.extend(cmds)
 
 if __name__ == '__main__':
 	main()
+	testExecute()
 
 
 
