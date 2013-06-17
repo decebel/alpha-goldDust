0c10f923263812aec35e6a7d12bf552626a29c7c
Decebel <decebel@gmail.com>
2013-06-17 00:48:17 -0400

    plugin working versions

--- bin/plug/py/sources/weby/WikipediaDataCommand.py
+++ bin/plug/py/sources/weby/WikipediaDataCommand.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.join(basePlugPath, "external"))
 
 # TODO - know difference between module import vs package import?
 import drawingboard
-from pattern.web import Twitter, hashtags
+from pattern.web import Wikipedia
 
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
@@ -14,7 +14,7 @@ print sys.modules["pattern.web"]
 #print sys.modules["DataCommand"]
 #pp.pprint(sys.modules)
 
-class TwitterDataCommand(drawingboard.DataCommand):
+class WikipediaDataCommand(drawingboard.DataCommand):
 	
 	def __init__(self, **kwargs):
 		drawingboard.DataCommand.__init__(self, **kwargs)
@@ -27,7 +27,7 @@ class TwitterDataCommand(drawingboard.DataCommand):
 		as it does this, it will keep calling status message. 
 		"""
 		print "loading "
-		self.set_icon(self, "twitter.icon")
+		self.set_icon(self, "Wikipedia.icon")
 
 
 	#def start(self, **kwargs):
@@ -42,8 +42,8 @@ class TwitterDataCommand(drawingboard.DataCommand):
 		Note: all these arguments can also be set by callin set_params with key=value pairs.
 		is_initialized will return true when all the required argum(ents are ready
 		"""
-		#self.set_display_name(self, kwargs.get("name", "twitter"))
-		self.args["engine"] = Twitter(language="en")
+		#self.set_display_name(self, kwargs.get("name", "wikipedia"))
+		self.args["engine"] = Wikipedia(language="en")
 						
 	def set_params(self, **kwargs):
 		pass
@@ -53,42 +53,28 @@ class TwitterDataCommand(drawingboard.DataCommand):
 
 	#def submit_command(self, port, **commandArgs):
 	def execute(self, **commandArgs):
-		searchString = commandArgs.get("search", "is cooler than") #from:decebel (from:username is also supported)
+		searchString = commandArgs.get("search", "life of pi") #from:decebel (from:username is also supported)
 		print("searching for {0}: ").format(searchString)
-		count = commandArgs.get("count", 25)
+		timeout = commandArgs.get("timeout", 25)
 		cached = commandArgs.get("cached", False)
-		return "skip skip "
-
-		# TODO: arg to decide if we want to do blocking or non-blocking 
-		# also, we should allow user to cancel pending requests
-		# also, we should auto-alert if the request is taking a long time
-		# TODO: we should have test cases for verifying these
 		engine = self.args["engine"]
-		result = {}
-		for tweet in engine.search(searchString, count=count, cached=cached):
-			tid = str(hash(tweet.author + tweet.date)) #  since the id is sometimes negative. TODO
-			print "# {2} Date={0}. Author= {1}".format(tweet.date, tweet.author, tid)			
-			rec = {"author" : tweet.author, "text" : tweet.text, "date" : tweet.date, "hashtags" : hashtags(tweet.text)}
-			#print "record = {0}".format(rec)
-			result[tweet.author] = rec
-			#print "I AM HERE"
-			#print "\nRESULT={0}".format(result)
-			#print "RECORD = {0}".format(rec)
-			#print "HOW MANY TIMES"
-			#pp.pprint(result)
+		article = engine.search(searchString, cached=cached, timeout=timeout)
 
-			#print tweet.text
-		pp.pprint(result)	
-		print "FOR LOOP COMPLETE"
+		print article.title
 
-		return result
+		for s in article.sections:
+			print s.title.upper()
+			print 
+			print s.content
+			print
 
-		
-		
+		return article.title
+
+	
 def main():
-	tw = TwitterDataCommand(dummy="dummy")	
-	tw.start(en="english")
-	res = tw.execute(search="keep your focus") 
+	wp = WikipediaDataCommand(dummy="dummy")	
+	wp.start(en="english")
+	res = wp.execute(search="Like of Pi") 
 	#for k in res:
 	#	print "key={0}".format(k)
 	pp.pprint(res)
